7aa3cb9e3c0466ae302e75d2ea67c5d6dc3dfc2c
rebeljah <rebeljah@gmail.com>
2024-06-11 18:11:35 -0400

    first commit

---
+++ pubsub.cpp
@@ -0,0 +1,73 @@
+#include "pubsub.hpp"
+
+
+ps::Subject::~Subject() {
+    for (auto kv : m_observers) {
+        SignalType signalType = kv.first;
+        ObserverSet &observers = kv.second;
+        for (auto pObserver : observers) {
+            pObserver->removeTrackingSubject(signalType, this);
+        }
+    }
+}
+
+
+void ps::Subject::addObserver(SignalType signalType, IObserver* observer) {
+    // init observer set for this signal type
+    if (m_observers.find(signalType) == m_observers.end()) {
+        m_observers.emplace(signalType, ObserverSet());
+    }
+
+    m_observers.at(signalType).emplace(observer);
+    observer->addTrackingSubject(signalType, this);
+}
+
+
+void ps::Subject::removeObserver(SignalType signalType, IObserver* observer) {
+    auto& observerSet = m_observers.at(signalType);
+    observerSet.erase(observer);
+
+    // erase this signal from map if subscriber was last observer
+    if (observerSet.empty())
+        m_observers.erase(signalType);
+}
+
+
+void ps::Subject::emitSignal(const Signal& signal) {
+    if (m_observers.find(signal.type) == m_observers.end())
+        return;  // signal has no observers so return
+
+    for (auto pObserver : m_observers.at(signal.type)) {
+        pObserver->onSignal(signal);
+    }
+}
+
+
+ps::IObserver::~IObserver() {
+    for (auto kv : m_subjects) {
+        SignalType signalType = kv.first;
+        SubjectSet &subjects = kv.second;
+        for (auto pSubject : subjects) {
+            pSubject->removeObserver(signalType, this);
+        }
+    }
+}
+
+
+void ps::IObserver::addTrackingSubject(SignalType signalType, Subject* subject) {
+    if (m_subjects.find(signalType) == m_subjects.end()) {
+        m_subjects.emplace(signalType, SubjectSet());
+    }
+
+    m_subjects.at(signalType).emplace(subject);
+}
+
+
+void ps::IObserver::removeTrackingSubject(SignalType signalType, Subject* subject) {
+    auto& subjectSet = m_subjects.at(signalType);
+    subjectSet.erase(subject);
+
+    // erase this signal from map if subscriber was last observer
+    if (subjectSet.empty())
+        m_subjects.erase(signalType);
+}
---
+++ pubsub.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include <unordered_set>
+
+using std::string;
+using std::map;
+using std::unordered_set;
+
+
+namespace ps
+{
+    enum class SignalType : int {
+        PlayerNameSet,  // .str
+        Click,          // .point
+        Quit,           // n/a
+    };
+
+    struct Signal {
+        SignalType type;
+        string str;  // always present, may be empty
+
+        struct Vec2I {
+            int x, y;
+        };
+
+        // refer to enum to check which meber to access
+        union {
+            Vec2I point;
+        };
+
+        Signal(SignalType type) : type(type) {}
+    };
+
+    class IObserver;
+
+    class Subject {
+        public:
+
+        virtual ~Subject();
+        virtual void addObserver(SignalType signalType, IObserver* observer);
+        virtual void removeObserver(SignalType signalType, IObserver* observer);
+
+        protected:
+
+        virtual void emitSignal(const Signal& signal);
+
+        private:
+
+        using ObserverSet = unordered_set<IObserver*>;
+        using ObserverMap = map<SignalType, ObserverSet>;
+        ObserverMap m_observers;
+    };
+
+
+    class IObserver {
+        public:
+
+        virtual ~IObserver();
+        virtual void addTrackingSubject(SignalType, Subject*);
+        virtual void removeTrackingSubject(SignalType, Subject*);
+        virtual void onSignal(const Signal&) = 0;
+
+        private:
+
+        using SubjectSet = unordered_set<Subject*>;
+        using SubjectMap = map<SignalType, SubjectSet>;
+        SubjectMap m_subjects;
+    };
+};  // namespace pubsub
